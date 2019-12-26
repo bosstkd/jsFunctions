daa251b03e193a25a0cb15f549ab3e26d8882a02
Amine <a-ek@hotmail.fr>
2019-12-26 23:46:30 +0100

    some modifications to use in my job don't take reference.

--- fct.js
+++ fct.js
@@ -1,164 +1,173 @@
+				//---------- Retourne un tableau des elements appeler par leurs tagName ex: "LI"
+			function getByElementsTagName(elementsTagName){
+			 return document.getElementsByTagName(elementsTagName);;
+			}
 
-//---------- Retourne un tableau des elements appeler par leurs tagName ex: "LI"
-function getByElementsTagName(elementsTagName){
- return document.getElementsByTagName(elementsTagName);;
+			//---------- Ajout d'identifiants aux element appeler par leurs tagName forme d'id: tagName_i 
+			//---------- Ex: "LI" => LI_0, LI_1 ... LI_N; 
+			function setElementsIdAsName(tags){
+				var elt = getByElementsTagName(tags);
+				for(var i = 0; i<elt.length; i++){
+					  elt[i].setAttribute("id", tags+"_"+i);
+				}
+			}
+
+			//---------- Ajout data-value aux element appeler par leurs tagName forme de data-value: tagName_i 
+			//---------- Ex: "LI" => LI_0, LI_1 ... LI_N; 
+			function setElementsDataValue(tags){
+				var elt = getByElementsTagName(tags);
+				for(var i = 0; i<elt.length; i++){
+					  elt[i].setAttribute("data-value", tags+"_"+i);
+				}
+			}
+
+
+			//-----------Identification---------------
+
+			//---retourn l'element par l'id---
+			function getById(i){
+			 return document.getElementById(i);
+			}
+
+			//---retourn la data-value par l'id---
+			function getDataValue(id) {
+			   return document.getElementById(id).getAttribute('data-value');
+			}
+
+			//---retourn le nom d'element par l'id---
+			function getElementName(id){
+			   return getById(id).name;
+			}
+			
+			//---retourn la valeur d'element par l'id---
+			function getElementValue(id){
+			   return getById(id).value;
+			}
+
+
+			//--------------CSS------------------------
+
+			//----Change la couleur de l'element par l'id
+			function changeElementColor(id, color){
+			   var elt = getById(id);
+			   elt.style.color = color;
+			}
+
+			//----Change la couleur, la borderColor, et la backgroundColor de l'element par l'id
+			function changeElementCSSColors(id, color, borderColor, backgroundColor){
+			   var elt = getById(id);
+			   elt.style.color = color;
+			   elt.style.borderColor = borderColor;
+			   elt.style.backgroundColor = backgroundColor;
+			}
+
+			// fontSize in pixel ex: '15px' fontFamily ex: 'Monospace' fontStyle ex: 'italic', fontWeight ex: 'bold'
+			function changeElementFont(id, fontSize, fontFamily, fontStyle, fontWeight){
+			   var elt = getById(id);
+			   elt.style.fontSize = fontSize;
+			   elt.style.fontFamily = fontFamily;
+			   elt.style.fontWeight = fontWeight;
+			}
+
+			//---- Change le style de la bordure de l'element par l'id
+			function changeElementBorderStyle(id, borderStyle, borderWidth, borderRadius){
+				  var elt = getById(id);
+				   elt.style.borderStyle = borderStyle;
+				   elt.style.borderWidth = borderWidth;
+				   elt.style.borderRadius = borderRadius;
+			}
+
+			//------------Formel-------------------
+
+			// Modifie la valeur de l'element appeler par l'id
+			function setElementValue(id, value){
+				 var elt = getById(id);
+				 elt.value = value;
+			}
+
+			function setElementInnerHtml(id, value){
+				 var elt = getById(id);
+				 elt.innerHTML = value;
+			}
+
+			// change l'etat de l'element entre enable/disable;
+			function setElementStatus(id, state){
+				var elt = getById(id);
+				elt.disabled = state;
+			}
+
+			// met un switch sur l'etat de l'element enable/disable.
+			function setElementStatuSwitch(id){
+				var elt = getById(id);
+				elt.disabled = !elt.disabled;
+			}
+
+			// met un switch sur (0:(visible), 1(cachet), 2(cachet et position éliminé)) par l'id.
+			function setElementVisibility(id, entier){
+				var x = document.getElementById(id);
+				
+				console.log(id+" "+entier);
+				
+					 if (entier === '0'){
+							x.style.visibility = 'visible';
+						  } else 
+					 if (entier === '1'){
+							x.style.visibility = 'hidden';
+						  }else{
+							x.style.visibility = 'collapse';
+						  }
+			}
+
+
+			//---------String traitement-------------------------------
+			function strMatcher(str, regex) {
+			  var res = str.match(regex);
+			  return res != null 
+			}
+
+			// liste de mots depuis un paragraph;
+			function getListStringFromParagraph(paragraph){
+				var regex = /[A-Za-z_]*/g;
+				var found = paragraph.match(regex);
+				var fList = [];
+				for(let i=0; i<found.length; i++){
+					if(found[i] != "") fList.push(found[i]);
+				}
+
+				return fList;
+			}
+
+			// liste d'entier depuis un paragraph;
+			function getListIntFromParagraph(paragraph){
+				var regex = /[0-9]*/g;
+				var found = paragraph.match(regex);
+				var fList = [];
+				for(let i=0; i<found.length; i++){
+					if(found[i] != "") fList.push(found[i]);
+				}
+
+				return fList;
+			}
+
+			// liste double depuis un paragraph;
+			function getListDoubleFromParagraph(paragraph){
+				var regex = /[0-9]*\.[0-9]*/g;
+				var found = paragraph.match(regex);
+				var fList = [];
+				for(let i=0; i<found.length; i++){
+					if(found[i] != "") fList.push(found[i]);
+				}
+
+				return fList;
+			}
+			
+var person = {
+  firstName:"John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
 }
 
-//---------- Ajout d'identifiants aux element appeler par leurs tagName forme d'id: tagName_i 
-//---------- Ex: "LI" => LI_0, LI_1 ... LI_N; 
-function setElementsIdAsName(tags){
-	var elt = getByElementsTagName(tags);
-	for(var i = 0; i<elt.length; i++){
-		  elt[i].setAttribute("id", tags+"_"+i);
-	}
-}
 
-//---------- Ajout data-value aux element appeler par leurs tagName forme de data-value: tagName_i 
-//---------- Ex: "LI" => LI_0, LI_1 ... LI_N; 
-function setElementsDataValue(tags){
-	var elt = getByElementsTagName(tags);
-	for(var i = 0; i<elt.length; i++){
-		  elt[i].setAttribute("data-value", tags+"_"+i);
-	}
-}
-
-
-//-----------Identification---------------
-
-//---retourn l'element par l'id---
-function getById(i){
- return document.getElementById(i);
-}
-
-//---retourn la data-value par l'id---
-function getDataValue(id) {
-   return document.getElementById(id).getAttribute('data-value');
-}
-
-//---retourn le nom d'element par l'id---
-function getElementName(id){
-   return getById(id).name;
-}
-
-//---retourn la valeur d'element par l'id---
-function getElementValue(id){
-   return getById(id).value;
-}
-
-
-//--------------CSS------------------------
-
-//----Change la couleur de l'element par l'id
-function changeElementColor(id, color){
-   var elt = getById(id);
-   elt.style.color = color;
-}
-
-//----Change la couleur, la borderColor, et la backgroundColor de l'element par l'id
-function changeElementCSSColors(id, color, borderColor, backgroundColor){
-   var elt = getById(id);
-   elt.style.color = color;
-   elt.style.borderColor = borderColor;
-   elt.style.backgroundColor = backgroundColor;
-}
-
-// fontSize in pixel ex: '15px' fontFamily ex: 'Monospace' fontStyle ex: 'italic', fontWeight ex: 'bold'
-function changeElementFont(id, fontSize, fontFamily, fontStyle, fontWeight){
-   var elt = getById(id);
-   elt.style.fontSize = fontSize;
-   elt.style.fontFamily = fontFamily;
-   elt.style.fontWeight = fontWeight;
-}
-
-//---- Change le style de la bordure de l'element par l'id
-function changeElementBorderStyle(id, borderStyle, borderWidth, borderRadius){
-      var elt = getById(id);
-	   elt.style.borderStyle = borderStyle;
-	   elt.style.borderWidth = borderWidth;
-	   elt.style.borderRadius = borderRadius;
-}
-
-//------------Formel-------------------
-
-// Modifie la valeur de l'element appeler par l'id
-function setElementValue(id, value){
-     var elt = getById(id);
-	 elt.value = value;
-}
-
-function setElementInnerHtml(id, value){
-     var elt = getById(id);
-	 elt.innerHTML = value;
-}
-
-// change l'etat de l'element entre enable/disable;
-function setElementStatus(id, state){
-	var elt = getById(id);
-	elt.disabled = state;
-}
-
-// met un switch sur l'etat de l'element enable/disable.
-function setElementStatuSwitch(id){
-	var elt = getById(id);
-	elt.disabled = !elt.disabled;
-}
-
-// met un switch sur (0:(visible), 1(cachet), 2(cachet et position éliminé)) par l'id.
-function setElementVisibility(id, entier){
-	var x = document.getElementById(id);
-	
-	console.log(id+" "+entier);
-	
-		 if (entier === '0'){
-				x.style.visibility = 'visible';
-			  } else 
-		 if (entier === '1'){
-				x.style.visibility = 'hidden';
-			  }else{
-				x.style.visibility = 'collapse';
-			  }
-}
-
-
-//---------String traitement-------------------------------
-function strMatcher(str, regex) {
-  var res = str.match(regex);
-  return res != null 
-}
-
-// liste de mots depuis un paragraph;
-function getListStringFromParagraph(paragraph){
-	var regex = /[A-Za-z_]*/g;
-	var found = paragraph.match(regex);
-	var fList = [];
-	for(let i=0; i<found.length; i++){
-		if(found[i] != "") fList.push(found[i]);
-	}
-
-	return fList;
-}
-
-// liste d'entier depuis un paragraph;
-function getListIntFromParagraph(paragraph){
-	var regex = /[0-9]*/g;
-	var found = paragraph.match(regex);
-	var fList = [];
-	for(let i=0; i<found.length; i++){
-		if(found[i] != "") fList.push(found[i]);
-	}
-
-	return fList;
-}
-
-// liste double depuis un paragraph;
-function getListDoubleFromParagraph(paragraph){
-	var regex = /[0-9]*\.[0-9]*/g;
-	var found = paragraph.match(regex);
-	var fList = [];
-	for(let i=0; i<found.length; i++){
-		if(found[i] != "") fList.push(found[i]);
-	}
-
-	return fList;
-}
  
